ca8c6a692959437d5960df4dc59fa88a0733c194
linronglang <272805749@qq.com>
2019-07-24 17:38:11 +0800

    feat(git cz): 添加git cz功能闺房代码提交

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,6 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set("@", resolve("src"));
   },
+  // 构建给github-pages
   publicPath: process.env.NODE_ENV === "production" ? "/vue-admin" : "/"
 };
